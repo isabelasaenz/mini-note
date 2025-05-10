170bcbedd49d69ce80931b17e7717973be054757
isabelasaenz <isabelasaenzcardoso@gmail.com>
2025-05-10 17:06:54 -0300

    Revert "eslint fix 2"
    This reverts commit 6814bef89c6d83e7510b70f55a58ce782d1799f6.

--- mini-note/eslint.config.mjs
+++ mini-note/eslint.config.mjs
@@ -10,11 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),{
-    rules: {
-      "@typescript-eslint/no-unused-vars" : "warn",
-    },
-  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
